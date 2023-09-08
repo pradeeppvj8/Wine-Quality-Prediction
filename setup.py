614fe983663fa0fbc337a2d714ae17c18eded90f
pradeeppvj8 <pradeep.pvj8@gmail,com>
2023-09-08 23:55:11 +0530

    setup.py & requirements.txt created

---
+++ setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+__version__ = "0.0.0"
+
+REPO_NAME = "Wine-Quality-Prediction"
+AUTHOR_NAME = "Pradeep P"
+AUTHOR_USER_NAME = "pradeeppvj8"
+SRC_REPO = "wqp_project"
+AUTHOR_EMAIL = "pradeep.pvj8@gmail.com"
+
+setuptools.setup(
+    name = SRC_REPO,
+    version = __version__,
+    author = AUTHOR_NAME,
+    author_email = AUTHOR_EMAIL,
+    description = "Wine Quality Prediction",
+    long_description = long_description,
+    long_description_content = "text/markdown",
+    url = f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls = {
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir = {"": "src"},
+    packages = setuptools.find_packages(where="src")
+)
